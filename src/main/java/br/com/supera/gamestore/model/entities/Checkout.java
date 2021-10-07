a8ee83b6a5991695ba21268c3ec1d2c1c34d9afe
Carlos Nascimento <cdgodoy17@gmail.com>
2021-10-07 16:10:51 -0300

    Corrigindo buscas

--- src/main/java/br/com/supera/gamestore/model/entities/Checkout.java
+++ src/main/java/br/com/supera/gamestore/model/entities/Checkout.java
@@ -1,4 +1,50 @@
 package br.com.supera.gamestore.model.entities;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Checkout {
+    private BigDecimal subTotal;
+    private BigDecimal frete;
+    private BigDecimal total;
+
+    public Checkout(BigDecimal subTotal, BigDecimal frete, BigDecimal total) {
+        this.subTotal = subTotal;
+        this.frete = frete;
+        this.total = total;
+    }
+
+    public BigDecimal getSubTotal() {
+        return subTotal;
+    }
+
+    public BigDecimal getFrete() {
+        return frete;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return "Checkout{" +
+                "subTotal=" + subTotal +
+                ", frete=" + frete +
+                ", total=" + total +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Checkout checkout = (Checkout) o;
+        return Objects.equals(subTotal, checkout.subTotal) && Objects.equals(frete, checkout.frete) && Objects.equals(total, checkout.total);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subTotal, frete, total);
+    }
 }

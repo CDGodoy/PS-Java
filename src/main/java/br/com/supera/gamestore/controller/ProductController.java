91efd6413ac451877ac75fd0168b3606dc65ea0f
Carlos Nascimento <cdgodoy17@gmail.com>
2021-10-09 16:32:52 -0300

    Melhorando frete gratis e escrevendo comentarios

--- src/main/java/br/com/supera/gamestore/controller/ProductController.java
+++ src/main/java/br/com/supera/gamestore/controller/ProductController.java
@@ -18,6 +18,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller da aplicação
+ */
 @RestController
 @RequestMapping
 public class ProductController {
@@ -25,18 +28,24 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    /*
-    public List < Product > obterProdutos() {
-        return productRepository.findAll();
-    }
-     */
+    //Lista que conterá os produtos que foram adicionados ao carrinho.
+    ArrayList < Product > carrinho = new ArrayList < > ();
 
+    /**
+     * Este método salva o produto no banco de dados.
+     * @param dto Produto e JSON.
+     * @return ResponseEntity retornando o objeto do produto inserido em JSON.
+     */
     @PostMapping("/produto")
     public ResponseEntity<ProductResponseDTO> salvarProduto(@RequestBody @Validated ProductDTO dto){
         Product product = productRepository.save(dto.transformaParaObjeto());
         return new ResponseEntity<>(ProductResponseDTO.transformaEmDTO(product), HttpStatus.CREATED);
     }
 
+    /**
+     * Este método obtém o todos os produtos do banco.
+     * @return ResponseEntity com todos os objetos do banco em JSON.
+     */
     @GetMapping(path = "/produto")
     public ResponseEntity<List<ProductResponseDTO>> obterTodosProdutos(){
         List<Product> products = productRepository.findAll();
@@ -46,6 +55,11 @@ public class ProductController {
         return new ResponseEntity<>(productsDTO, HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Este método obtém todos os produtos do banco com uma ordem definida.
+     * @param atributo Atributo que será utilizado como referência para ordenar em ordem crescente.
+     * @return ResponseEntity com todos os objetos do banco em ordem em JSON.
+     */
     @GetMapping(path = "/produto/sort")
     public ResponseEntity<List<ProductResponseDTO>> obterProdutos (@RequestParam String atributo){
         List<Product> products = productRepository.findAll(Sort.by(Sort.Direction.ASC, atributo));
@@ -55,6 +69,13 @@ public class ProductController {
         return new ResponseEntity<>(productsDTO, HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Este método obtém todos os produtos do banco com uma ordem definida e separado por páginas com tamanho definido.
+     * @param atributo Atributo que será utilizado como referência para ordenar em ordem crescente.
+     * @param page Número da página (começando em 0).
+     * @param itens Quantidade de itens a serem exibidos por página.
+     * @return Página solicitada.
+     */
     @GetMapping(path = "/produto/pagina/{page}")
     public Iterable<Product> obterProdutosPorPagina(@RequestParam String atributo,
                                                     @PathVariable int page,
@@ -64,6 +85,11 @@ public class ProductController {
         return productRepository.findAll(pagina);
     }
 
+    /**
+     * Este método obtém o produto por um determinado ID. Caso o id não exista, retorna um produto vazio.
+     * @param id do produto a ser pesquisado.
+     * @return produto.
+     */
     public Product obterProdutoId(Long id){
         if(!productRepository.existsById(id)){
             return new Product();
@@ -71,6 +97,11 @@ public class ProductController {
         return productRepository.findById(id).get();
     }
 
+    /**
+     * Este método obtém o produto por um determinado ID. Chamado através de uma requisição GET.
+     * @param id id do produto a ser pesquisado.
+     * @return ResponseEntity com o objeto encontrado.
+     */
     @GetMapping(path = "/produto/{id}")
     public ResponseEntity<ProductResponseDTO> obterProdutoIdResp(@PathVariable Long id){
 
@@ -79,8 +110,12 @@ public class ProductController {
         return new ResponseEntity<>(produto, HttpStatus.ACCEPTED);
     }
 
-    ArrayList < Product > carrinho = new ArrayList < > ();
 
+    /**
+     * Este método adiciona um produto ao carrinho a partir do seu id.
+     * @param id id do produto a ser pesquisado e inserido no carrinho.
+     * @return ResponseEntity com o produto adicionado no carrinho.
+     */
     @PostMapping(path = "produto/add")
     public ResponseEntity<ProductResponseDTO> addCarrinho(@RequestParam Long id){
 
@@ -90,24 +125,34 @@ public class ProductController {
         return new ResponseEntity<>(ProductResponseDTO.transformaEmDTO(produto), HttpStatus.CREATED);
     }
 
+    /**
+     * Este método deleta um ítem do carrinho a partir do seu id.
+     * @param id id do produto a ser pesquisado e deletado do carrinho.
+     * @return ResponseEntity com o produto que foi deletado do carrinho.
+     */
     public ResponseEntity<ProductResponseDTO> delItemCarrinho(@PathVariable Long id){
         Product produto = obterProdutoId(id);
         carrinho.remove(produto);
 
         return new ResponseEntity<>(ProductResponseDTO.transformaEmDTO(produto), HttpStatus.CREATED);
     }
-    
+
+    /**
+     * Este método obtém os itens que foram adicionados ao carrinho.
+     * @return ResponseEntity com os itens do carrinho.
+     */
     @GetMapping(path = "/carrinho")
     public ResponseEntity<List<Product>> obterCarrinho(){
         return new ResponseEntity<>(carrinho, HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Este método calcula e exibe os valores do pedido no checkout.
+     * @return CheckoutService.
+     */
     @GetMapping(path = "/checkout")
     public ResponseEntity<Checkout> Checkout(){
-
-        Checkout checkout = CheckoutService.obterCheckout(carrinho);
-
-        return new ResponseEntity<>(checkout, HttpStatus.ACCEPTED);
+        return CheckoutService.obterCheckout(carrinho);
     }
 
 
--- src/main/java/br/com/supera/gamestore/service/CheckoutService.java
+++ src/main/java/br/com/supera/gamestore/service/CheckoutService.java
@@ -2,6 +2,8 @@ package br.com.supera.gamestore.service;
 
 import br.com.supera.gamestore.model.entities.Checkout;
 import br.com.supera.gamestore.model.entities.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import javax.validation.constraints.Digits;
 import java.math.BigDecimal;
@@ -9,29 +11,38 @@ import java.util.List;
 
 public class CheckoutService {
 
-    public static Checkout obterCheckout(List<Product> carrinho){
-        BigDecimal subTotal = new BigDecimal("0");
+    /**
+     * Este método efetua o cálculo do checkout.
+     * @param carrinho Carrinho com os produtos adicionados.
+     * @return ResponseEntity com
+     */
+    public static ResponseEntity<Checkout> obterCheckout(List<Product> carrinho){
+        BigDecimal subTotal = new BigDecimal("0"); //Criando valor zerado de um subtotal
         @Digits(integer = 1, fraction = 2)
-        BigDecimal frete = new BigDecimal("0");
-        BigDecimal freeFrete = new BigDecimal("250");
-        BigDecimal total;
+        BigDecimal frete = new BigDecimal("0"); //Criando um valor zerado do frete
+        BigDecimal totalCompra;
+        boolean freteGratis = false;
 
+        /* Este ciclo for percorre todos os itens do carrinho e a cada produto, acrescenta 10.0
+         * ao frete e adiciona o valor do produto ao subTotal.
+         */
         for (Product p:
                 carrinho) {
             frete = frete.add(BigDecimal.TEN);
             subTotal = subTotal.add(new BigDecimal(String.valueOf(p.getPrice())));
         }
 
-        if (subTotal.compareTo(freeFrete) > 0) {
+        //Caso o subtotal seja maior que 250, o frete será multiplicado por 0, para ser grátis
+        if (subTotal.compareTo(new BigDecimal("250")) > 0) {
             frete = frete.multiply(BigDecimal.ZERO);
+            freteGratis = true;
         }
 
-        total = subTotal.add(frete);
+        totalCompra = subTotal.add(frete);
 
-        Checkout checkout = new Checkout(subTotal, frete, total);
+        Checkout checkout = new Checkout(subTotal, frete, totalCompra, freteGratis);
 
-        return checkout;
-
+        return new ResponseEntity<>(checkout, HttpStatus.ACCEPTED);
     }
 
 }
